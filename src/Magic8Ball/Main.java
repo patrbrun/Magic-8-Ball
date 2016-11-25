ebae6f22361dca8004299a9531c97fcd85624525
Patricia Bruno Barbosa <paaat29@gmail.com>
2016-11-25 12:42:45 +0200

    Test

---
+++ src/Magic8Ball/Main.java
@@ -0,0 +1,27 @@
+package Magic8Ball;
+
+import lib.TextIO;
+
+/**
+ * Created by pati on 25.11.16.
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        String userInput;  // The question input by the user.
+        System.out.print("What is your question?");
+        userInput = TextIO.getlnString();
+
+        System.out.println();
+
+        while {
+
+        }
+        System.out.println("Your question is " + ""userInput);
+        System.out.println();
+
+    }
+
+    }
